9ad13ce1814ed7cfbd5f64a7afac60dad27f34ad
Magnussiiftun1857 <39226927@qq.com>
2018-01-31 13:44:02 +0800

    update files

---
+++ python/notes.py
@@ -0,0 +1,26 @@
+#单行注释
+'''
+多
+行
+注
+释
+'''
+"""
+多
+行
+注
+释
+"""
+
+age = 8
+
+if age >= 18:
+    print("adult")
+else:
+    print("not adult")
+
+print(r'''hello,\n
+world''')
+
+if True or not False:
+    print("True or not false")
